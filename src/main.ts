05ff136532580e5f1136c3d84207932b85497eb5
李浩坚 <751609117@qq.com>
2022-01-11 17:51:25 +0800

    feat：配置swagger接口文档

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './core/filter/http-exception.filter';
 import { TransformInterceptor } from './core/interceptor/transform.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 设置swagger文档
+  const config = new DocumentBuilder()
+    .setTitle('nest-test')   
+    .setDescription('nest接口文档')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.setGlobalPrefix('api'); // 设置全局路由前缀
   app.useGlobalFilters(new HttpExceptionFilter()); // 全局注册错误的过滤器
   app.useGlobalInterceptors(new TransformInterceptor()); // 全局注册成功拦截器
